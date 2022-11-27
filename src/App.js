0077dc47308fe6cd500dbfa815a1e30fd093297d
Nir Wilner <nir.w@fmy.company>
2022-11-27 13:04:52 +0200

    empty template for the react assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useEffect, useState } from "react";
+import { useWindowDimensions, adjustContainer } from "./useWindowDimensions";
+import WebAnalysisItems from "./components/WebAnalysisItems";
+import { Row, Col } from "react-bootstrap";
+import SideTaskBar from "./components/SideTaskBar";
+import TopTaskBar from "./components/TopTaskBar";
+
+//DONT TOUCH THIS FILE
+
+//static screen ratio dimentions
+export const staticWidth = 2250;
+export const staticHeight = 1250;
 
 function App() {
+
+  //get current screen dimentions
+  const {height, width} = useWindowDimensions();
+
+  //deal with every change in dimentions to keep the screen ratio
+  useEffect(() => {
+    adjustContainer(height, width);
+  }, [height, width]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="outer">
+        <div className="middle">
+            <div className="websiteContainer">
+              
+              <TopTaskBar />
+              <Row>
+                <Col sm={3} className="position-fixed flex-fill" id="sidebarCol">
+                  <SideTaskBar />
+                </Col>
+                <Col sm={9} className="content">
+                  <WebAnalysisItems />
+                </Col>
+              </Row>
+
+            </div>
+        </div>
+      </div>
+
   );
 }
 
 export default App;
+
+
+
---
+++ src/components/WebAnalysisItems.js
@@ -0,0 +1,12 @@
+
+//Write your code here, if needed, create more componnents
+
+function WebAnalysisItems() {
+
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default WebAnalysisItems;
